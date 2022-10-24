e7c89db0d3c53c790d3158a550c2a9c2f7b90ec0
Bruno Del Piero <bdelpiero93@gmail.com>
2022-10-24 09:59:36 -0300

    Add sushi operator to polygon

--- deploy/04-deploy-sushi-operator.js
+++ deploy/04-deploy-sushi-operator.js
@@ -4,10 +4,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   await deploy("SushiOperator", {
     from: deployer,
     args: [
-      "0x1421bDe4B10e8dd459b3BCb598810B1337D56842", // router
+      // Celo
+      //"0x1421bDe4B10e8dd459b3BCb598810B1337D56842", // router
+      //"0xc35DADB65012eC5796536bD9864eD8773aBc74C4", // factory
+      //"0x8084936982D089130e001b470eDf58faCA445008", // minichef
+      //"0x08A588dfaF7C2AA3a96bc16dd7df3704915b1E49", // rewarder
+
+      // Sushi
+      "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", // router
       "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", // factory
-      "0x8084936982D089130e001b470eDf58faCA445008", // minichef
-      "0x08A588dfaF7C2AA3a96bc16dd7df3704915b1E49", // rewarder
+      "0x0769fd68dFb93167989C6f7254cd0D766Fb2841F", // minichef
+      "0xdd21a7428e6da8a10f4A23142CE81EC21aCCE449", // rewarder
     ],
     log: true,
   });
--- deploy/06-deploy-operator-proxy.js
+++ deploy/06-deploy-operator-proxy.js
@@ -4,8 +4,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   await deploy("OperatorProxy", {
     from: deployer,
     args: [
-      "0x76cAD835648Edee3d460Ba8c48517f6924A6CF4B", // SushiOperator
-      "0x3C655Fd232B11504C778021c304010f5aAF68958", // UbeOperator
+      // Celo
+      //"0x76cAD835648Edee3d460Ba8c48517f6924A6CF4B", // SushiOperator
+      //"0x3C655Fd232B11504C778021c304010f5aAF68958", // UbeOperator
+
+      // Polygon
+      "0xDDdF7fC64b1ABC55fdEC219D1b20411ce9776479", // SushiOperator
+      "0x0D57f319f4539D51e2cE792D99f565828C2AD043" // UbeOperator
     ],
     log: true,
   });
