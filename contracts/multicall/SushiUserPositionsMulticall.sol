d5e0e0554739b7d3566d131af3da36eb328198c0
Gonza Nardini <nardinigonzalom@gmail.com>
2022-10-26 18:04:14 -0300

    Sushi multicall changes

--- contracts/multicall/SushiUserPositionsMulticall.sol
+++ contracts/multicall/SushiUserPositionsMulticall.sol
@@ -7,45 +7,55 @@ import "../interfaces/sushiswap/IRewarder.sol";
 import "../interfaces/uniswap/IUniswapV2Pair.sol";
 
 contract SushiUserPositionsMulticall {
-  struct SushiPosition {
-    address poolAddress;
-    uint256 share;
-    IERC20[] rewardTokens;
-    uint256[] rewardAmounts;
-    uint256 pendingSushi;
-  }
-
-  function getPositions(
-    IMiniChefV2 miniChef,
-    IRewarder rewarder,
-    address owner
-  ) external view returns (SushiPosition[] memory) {
-    uint256 poolCount = miniChef.poolLength();
-
-    SushiPosition[] memory positions = new SushiPosition[](poolCount);
-    for (uint256 index = 0; index < poolCount; index++) {
-      (uint256 amount, ) = miniChef.userInfo(index, owner);
-      if (amount == 0) {
-        continue;
-      }
-      address lpToken = miniChef.lpToken(index);
-      IUniswapV2Pair pair = IUniswapV2Pair(lpToken);
-      uint256 totalSupply = pair.totalSupply();
-      uint256 share = (amount * 10**18) / totalSupply;
-
-      uint256 pendingSushi = miniChef.pendingSushi(index, owner);
-      (IERC20[] memory rewardTokens, uint256[] memory rewardAmounts) = rewarder
-        .pendingTokens(index, owner, pendingSushi);
-
-      positions[index] = (SushiPosition(
-        lpToken,
-        share,
-        rewardTokens,
-        rewardAmounts,
-        pendingSushi
-      ));
+    struct SushiPosition {
+        address poolAddress;
+        uint256 share;
+        IERC20[] rewardTokens;
+        uint256[] rewardAmounts;
+        uint256 pendingSushi;
     }
 
-    return positions;
-  }
+    function getPositions(
+        IMiniChefV2 miniChef,
+        address rewarder,
+        address owner
+    ) external view returns (SushiPosition[] memory) {
+        uint256 poolCount = miniChef.poolLength();
+
+        SushiPosition[] memory positions = new SushiPosition[](poolCount);
+        for (uint256 index = 0; index < poolCount; index++) {
+            (uint256 amount, ) = miniChef.userInfo(index, owner);
+            if (amount == 0) {
+                continue;
+            }
+            address lpToken = miniChef.lpToken(index);
+            IUniswapV2Pair pair = IUniswapV2Pair(lpToken);
+            uint256 totalSupply = pair.totalSupply();
+            uint256 share = (amount * 10**18) / totalSupply;
+
+            uint256 pendingSushi = miniChef.pendingSushi(index, owner);
+            (
+                IERC20[] memory rewardTokens,
+                uint256[] memory rewardAmounts
+            ) = rewarder != address(0)
+                    ? IRewarder(rewarder).pendingTokens(
+                        index,
+                        owner,
+                        pendingSushi
+                    )
+                    : (new IERC20[](0), new uint256[](0));
+
+            positions[index] = (
+                SushiPosition(
+                    lpToken,
+                    share,
+                    rewardTokens,
+                    rewardAmounts,
+                    pendingSushi
+                )
+            );
+        }
+
+        return positions;
+    }
 }
